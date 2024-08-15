293e4ae5411615d2a85bbf0368a76a260fbac82b
unknown <mosarofhossen5228@gmail.com>
2024-08-15 17:57:16 +0600

    data load

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
   return (
     <div>
-      <h2>This is left side nav</h2>
+      <h2 className="text-xl font-bold">All Category</h2>
+      <div className="px-2 space-y-2 mt-4 font-semibold">
+        {categories.map((category) => (
+          // <h2 key={category.id}>{category.name}</h2>
+          <Link
+            className="block"
+            key={category.id}
+            to={`/category/${category.id}`}
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
